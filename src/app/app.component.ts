82ade0f23bd9b5037795b6f0fc85a3c15d81e4a3
Juanjo <juanjotorres90@gmail.com>
2019-10-05 17:36:33 +0200

    Initial setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+declare var $: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'nba-players';
+
+ngOnInit() {
+  // tslint:disable-next-line: only-arrow-functions
+  $(function() {
+    $('[data-toggle="popover"]').popover();
+  });
+}
+
 }
